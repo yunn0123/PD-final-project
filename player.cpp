f81aa43f6e03bd6eb04f33e3b7ed0a71b64076eb
MirandaLiao <104979454+MirandaLiao@users.noreply.github.com>
2023-11-30 12:10:45 +0800

    add chloiceList for storing player's choices

--- player.cpp
+++ player.cpp
@@ -18,6 +18,7 @@ string printChange(int change){
 class Player {
 private:
     //TODO: itemList 存拿到的隱藏道具
+    int choiceList[100] = {0};   //針對有兩個選項的Card，用陣列存player從遊戲一開始到現在的選擇(1為左，2為右) ->處理連續卡牌
 public:
     int value1, value2, value3, value4; // 四個主要數值
     Player() : value1(INI_VAL), value2(INI_VAL), value3(INI_VAL), value4(INI_VAL) {}
