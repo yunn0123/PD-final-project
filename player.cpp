e72689572533003441534d66febcff2484be396a
yunn0123 <150316361+yunn0123@users.noreply.github.com>
2023-12-07 15:19:24 +0800

    Add files via upload

--- player.cpp
+++ player.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <cstdlib>
-#include <vector>
+#include "ALLimport.h"
 using namespace std;
 
 const int INI_VAL = 10;
-string printChange(int change);//印出數值變化
-
 string printChange(int change){
     string out = "";
     if(change > 0){
@@ -16,46 +14,23 @@ string printChange(int change){
     }
     return out;
 }
-class Player {
-public:
-    //TODO: itemList 存拿到的隱藏道具
-    int choiceList[100] = {0};   //針對有兩個選項的Card，用陣列存player從遊戲一開始到現在的選擇(1為左，2為右) ->處理連續卡牌
-    int value1, value2, value3, value4; // 四個主要數值
-    vector<int> chV1, chV2, chV3, chV4; //存四個數值遊戲期間變化
+Player :: Player() : value1(INI_VAL), value2(INI_VAL), value3(INI_VAL), value4(INI_VAL) {}
 
-    Player() : value1(INI_VAL), value2(INI_VAL), value3(INI_VAL), value4(INI_VAL) {
-        chV1.push_back(INI_VAL);
-        chV2.push_back(INI_VAL);
-        chV3.push_back(INI_VAL);
-        chV4.push_back(INI_VAL);
-    }
-
-    void updateValues(int change1, int change2, int change3, int change4); //更新玩家的數值並印出
-    // char getChoice(); //return玩家的選擇 ("l" or "r" or 其他)
-    // void useItem(); //TODO: 從itemList使用道具
-    // void catchItem(); //TODO: 拿到道具，存進itemList
-
-};
-void Player::updateValues(int change1, int change2, int change3, int change4) {
+void Player :: updateValues(int change1, int change2, int change3, int change4) 
+{
     value1 += change1;
     value2 += change2;
     value3 += change3;
     value4 += change4;
-    chV1.push_back(value1);
-    chV2.push_back(value2);
-    chV3.push_back(value3);
-    chV4.push_back(value4);
     cout << "A: " << value1 << printChange(change1) << ' ';
     cout << "B: " << value2 << printChange(change2) << ' ';
     cout << "C: " << value3 << printChange(change3) << ' ';
-    cout << "D: " << value4 << printChange(change4) << ' ' << endl;
-
+    cout << "D: " << value4 << printChange(change4) << ' ';
+    cout << endl;
     //checkForEndGame() by Ending?
 }
-// char Player::getChoice(){
-//     char choice;
-//     cin >> choice;
-//     return choice;
-// }
-
-
+char Player :: getChoice(){
+    char choice;
+    cin >> choice;
+    return choice;
+}
