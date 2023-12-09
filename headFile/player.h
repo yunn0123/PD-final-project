59601687b684fcd5215c9e86ca1dd10ad906f41b
MirandaLiao <104979454+MirandaLiao@users.noreply.github.com>
2023-12-09 19:01:33 +0800

    Update player.h

--- headFile/player.h
+++ headFile/player.h
@@ -19,11 +19,12 @@ class Player {
     private:
         //TODO: itemList 存拿到的隱藏道具
         int choiceList[100] = {0};   //針對有兩個選項的Card，用陣列存player從遊戲一開始到現在的選擇(1為左，2為右) ->處理連續卡牌
+        vector<int> chV1, chV2, chV3, chV4;
     public:
         int value1, value2, value3, value4; // 四個主要數值
         Player();
 
-        void updateValues(int change1, int change2, int change3, int change4); //更新玩家的數值並印出
+        void updateValues(int change1, int change2, int change3, int change4, Game& game); //更新玩家的數值並印出
         char getChoice(); //return玩家的選擇 ("l" or "r" or 其他)
         void useItem(); //TODO: 從itemList使用道具
         void catchItem(); //TODO: 拿到道具，存進itemList
