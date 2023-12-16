c69eeb951500b1373a101c610f68a703ab9937c0
BensonTheCPA <144212993+BensonTheCPA@users.noreply.github.com>
2023-12-16 23:02:20 +0800

    修正版1216

--- game1.cpp
+++ game1.cpp
@@ -1,42 +1,48 @@
-#include "./headFile/game1.h"
+#include "./headFile/game.h"
 using namespace std;
 
 Game :: Game(vector<NormalCard> nCard, vector<RandomCard> rCard, vector<EventCard> eCard, Player PLAYER)
 {
-	normalCard = nCard;
-	randomCard = rCard;
-	eventCard = eCard;
-	nCardIdx = -1;
-	rCardIdx = -1;
-	eCardIdx = -1;
-	nQuestionIdx = -1;
-	rQuestionIdx = -1;
-	eQuestionIdx = -1;
+	normalCard.assign(nCard.begin(), nCard.end());//try: normalCard = nCard; 
+	randomCard.assign(rCard.begin(), rCard.end());
+	eventCard.assign(eCard.begin(), eCard.end());
+	nCardIdx = 0;
+	rCardIdx = 0;
+	eCardIdx = 0;
 }
  
 void Game :: displayQuestion()
 {
-	if(normalCard[this->nCardIdx].nowQuestion < normalCard[this->nCardIdx].questionCnt)
-	{
-		normalCard[this->nCardIdx].GameCallingPrint();
-		normalCard[this->nCardIdx].nowQuestion += 1;
-	}
-	this->nCardIdx  += 1;
+	normalCard[this->nCardIdx].gameCallPrint();
 }
 
 void Game :: getChoice()
 {
-    if(GetAsyncKeyState(VK_LEFT) && 0x8001){ // choose left
-        keybd_event(VK_LEFT, 0, KEYEVENTF_KEYUP, 0);
-        nowChoice = 1;
-        PLAYER.updateValues(totalOpt[nowQuestion].val1[0], totalOpt[nowQuestion].val1[1], totalOpt[nowQuestion].val1[2], totalOpt[nowQuestion].val1[3]);
-        break;
+    while(true){
+        if(GetAsyncKeyState(VK_LEFT) && 0x8001){ // choose left
+            keybd_event(VK_LEFT, 0, KEYEVENTF_KEYUP, 0);
+            normalCard[this->nCardIdx].nowChoice = 1;
+            PLAYER.updateValues(normalCard[this->nCardIdx].totalOpt[nowQuestion].val1[0], normalCard[this->nCardIdx].totalOpt[nowQuestion].val1[1], normalCard[this->nCardIdx].totalOpt[nowQuestion].val1[2], normalCard[this->nCardIdx].totalOpt[nowQuestion].val1[3]);
+            break;
+        }
+        if(GetAsyncKeyState(VK_RIGHT) && 0x8001){ // choose right
+            keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);
+            normalCard[this->nCardIdx].nowChoice = 2;
+            PLAYER.updateValues(normalCard[this->nCardIdx].totalOpt[nowQuestion].val2[0], normalCard[this->nCardIdx].totalOpt[nowQuestion].val2[1], normalCard[this->nCardIdx].totalOpt[nowQuestion].val2[2], normalCard[this->nCardIdx].totalOpt[nowQuestion].val2[3]);
+            break;
+        }
     }
-    if(GetAsyncKeyState(VK_RIGHT) && 0x8001){ // choose right
-        keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);
-        nowChoice = 2;
-        PLAYER.updateValues(totalOpt[nowQuestion].val2[0], totalOpt[nowQuestion].val2[1], totalOpt[nowQuestion].val2[2], totalOpt[nowQuestion].val2[3]);
-
-        break;
+    cout << "-------------------" << endl;
+    // for next print
+    normalCard[this->nCardIdx].nowQuestion += (normalCard[this->nCardIdx].nowQuestion + normalCard[this->nCardIdx].nowChoice);
+    // if last question has asked, return to first question
+    if(normalCard[this->nCardIdx].nowQuestion >= normalCard[this->nCardIdx].questionCnt){
+        normalCard[this->nCardIdx].nowQuestion = 0;
+        normalCard[this->nCardIdx].nowChoice = 0;
+    }
+    // if in the array but no question
+    else if(normalCard[this->nCardIdx].totalOpt[nowQuestion].question.compare(" ") == 0){
+        normalCard[this->nCardIdx].nowQuestion = 0;
+        normalCard[this->nCardIdx].nowChoice = 0;
     }
 }
