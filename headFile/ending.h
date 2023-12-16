58ab26ae78d298478df0285f95dc22c6f5b7a7d8
panisleepy <151904238+panisleepy@users.noreply.github.com>
2023-12-16 15:08:11 +0800

    Update ending.h

--- headFile/ending.h
+++ headFile/ending.h
@@ -15,23 +15,23 @@ using namespace std;
 class Ending {
     public:
         // 檢查並觸發遊戲結局
-        static void checkForEnding(Player& player, Game& game);
+        void checkForEnding(Player& player, Game& game);
         // 重啟結局
-        static void restartEnding();
+        void restartEnding();
         // 普通結局
-        static void normalEnding();
+        void normalEnding();
         // 隱藏結局
-        static void hiddenEnding();
+        void hiddenEnding();
 
     private:
         // 檢查玩家數值是否平衡
-        static bool isValuesBalanced(Player& player, int minVal, int maxVal);
+        bool isValuesBalanced(Player& player, int minVal, int maxVal);
         // 檢查是否達到普通結局條件
-        static bool isNormalEndingAchieved(Game& game);
+        bool isNormalEndingAchieved(Game& game);
         // 檢查是否解鎖隱藏結局
-        static bool isHiddenEndingUnlocked(Player& player);
+        bool isHiddenEndingUnlocked(Player& player);
         // 檢查是否有特定道具
-        static bool hasSpecificItem(const Character& character, const string& itemName);
+        bool hasSpecificItem(const Character& character, const string& itemName);
 
 
 };
